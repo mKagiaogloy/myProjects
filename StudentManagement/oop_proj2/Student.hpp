dcd5d21e3c7115e41e06c65aa82eef0d0a5e9e9d
Mixalis Kagiaogloy <mkagiaogloy@gmail.com>
2024-04-05 20:43:59 +0300

    Add files via upload

---
+++ StudentManagement/oop_proj2/Student.hpp
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include "course.hpp"
+using namespace std;
+
+class Student
+{
+private:
+    // αριθμός μητρώου,ονοματεπώνυμο,εξάμηνο,διδακτικές μονάδες
+    string id;
+    string fullname;
+    int semester;
+    int credits;
+    int credits_left;
+    vector <Course> courses;
+public:
+    Student(string sid, string sfullname, int ssemester, int scredits);
+
+    void add_course(Course& c);
+    string get_id();
+    string get_name();
+    int get_semester();
+    int get_credits();
+    int get_credits_left();
+    vector <Course> get_courses();
+    bool is_course_valid(const Course &c);
+    friend ostream& operator<<(ostream& os, const Student& s);
+
+    // Setters
+    void set_id(string sid);
+    void set_name(string sfullname);
+    void set_semester(int ssemester);
+    void set_credits(int scredits);
+};
